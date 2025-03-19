ff28a8f882dd204b2daea7eb8bbd76f7a33a6b0b
nezeracuk <skibickignazar@gmail.com>
2025-03-19 15:06:54 +0200

    Auth for phones fix

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -271,14 +271,19 @@ const Navbar = () => {
               </Link>
             )}
             {!session && (
-              <button
-                key={provider.id}
-                onClick={() => signIn(provider.id)}
-                className="flex items-center text-white bg-gray-700 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2 my-5"
-              >
-                <FaGoogle className="text-white mr-2" />
-                <span>Login or Register</span>
-              </button>
+              <div>
+                {providers &&
+                  Object.values(providers).map((provider) => (
+                    <button
+                      key={provider.id}
+                      onClick={() => signIn(provider.id)}
+                      className="flex items-center text-white bg-gray-700 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2 my-5"
+                    >
+                      <FaGoogle className="text-white mr-2" />
+                      <span>Login or Register</span>
+                    </button>
+                  ))}
+              </div>
             )}
           </div>
         </div>
