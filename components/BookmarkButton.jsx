0efe38e0579219ea184a00dd76fed53414f0792d
nezeracuk <skibickignazar@gmail.com>
2025-03-19 14:54:25 +0200

    Auth for phones fix, unnecessary loading remove

--- components/BookmarkButton.jsx
+++ components/BookmarkButton.jsx
@@ -42,13 +42,6 @@ const BookmarkButton = ({ property }) => {
     });
   };
 
-  if (loading)
-    return (
-      <p className="bg-yellow-400 text-white font-bold w-full py-2 px-4 rounded-full flex items-center justify-center">
-        Loading...
-      </p>
-    );
-
   return isBookmarked ? (
     <button
       className="bg-red-500 hover:bg-red-600 text-white font-bold w-full py-2 px-4 rounded-full flex items-center justify-center"
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -271,7 +271,11 @@ const Navbar = () => {
               </Link>
             )}
             {!session && (
-              <button className="flex items-center text-white bg-gray-700 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2 my-5">
+              <button
+                key={provider.id}
+                onClick={() => signIn(provider.id)}
+                className="flex items-center text-white bg-gray-700 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2 my-5"
+              >
                 <FaGoogle className="text-white mr-2" />
                 <span>Login or Register</span>
               </button>
